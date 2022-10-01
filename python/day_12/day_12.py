cce1f1c77e3b1c2ff60072cc9ea70139f15c1f8f
Don-Burns <donal.w.burns@hotmail.com>
2022-10-01 17:48:25 +0100

    Move python into subdir

---
+++ python/day_12/day_12.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+input_file = 'validation_input.txt'
+
+def parse_input():
+    pass
+
+class Day12:
+    def __init__(self) -> None:
+        self.input = parse_input(input_file)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    input_file = 'input.txt'
+    main()
+
---
+++ python/day_16/test_day_16.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pytest
+import pandas as pd
+from random import randint
+from day_16 import Day16, parse_input
+class TestDay:
+
+    def test_input(self, input_path = 'validation_input.txt'):
+        ind = randint(0,5)
+        assert pd.read_csv(input_path, header=None).loc[ind,0] == parse_input(input_path).loc[ind,0]
